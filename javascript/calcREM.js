9514d8cf0cf553fb9c84aa36f9eef594469d3d85
meiseayoung <meiseayoung@gmail.com>
2016-11-26 11:00:55 +0800

    Update calcREM.js
    针对设备像素缩放比设置根元素的font-size

--- javascript/calcREM.js
+++ javascript/calcREM.js
@@ -1,6 +1,13 @@
 import $ from "jquery";
 
 (function (doc, win) {
+	var devicePixelRatio = (function(){
+		if(window.devicePixelRatio && typeof(window.devicePixelRatio) === "number" ){
+			return window.devicePixelRatio;
+		}else{
+			return 1;
+		}
+	})();
 	var docEl = doc.documentElement,
         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
         recalc = function () {
@@ -8,7 +15,7 @@ import $ from "jquery";
         	if(mainWidth<1366)mainWidth = 1366;
         	if(mainWidth>1920)mainWidth = 1920;
 			if (!mainWidth) return;
-            docEl.style.fontSize = 100 * (mainWidth / 1920) + 'px';
+            docEl.style.fontSize = 100 * (mainWidth / 1920) * devicePixelRatio + 'px';
         };
     recalc(); 
     win.addEventListener(resizeEvt, recalc, false);
