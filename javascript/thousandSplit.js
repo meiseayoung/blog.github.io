20d2b15fb2fb73f980906c335726fdd999e759a7
meiseayoung <meiseayoung@gmail.com>
2016-12-08 15:16:21 +0800

    Update thousandSplit.js

--- javascript/thousandSplit.js
+++ javascript/thousandSplit.js
@@ -1,5 +1,5 @@
 function thousandSplit(number,splitSymbol) {
-	return number.toString().replace(/(\d)(?=(\d{3})+\.)/g, function($0, $1) {
+	return (number+"").replace(/(\d)(?=(\d{3})+\.)/g, function($0, $1) {
 		return $1 + (splitSymbol || ",");
 	});
 }
