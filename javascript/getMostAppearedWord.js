0ff19fb71b7b92e3a65bca9177dcc3b6f7dc8f38
meiseayoung <meiseayoung@gmail.com>
2016-12-19 17:43:27 +0800

    Update getMostAppearedWord.js

--- javascript/getMostAppearedWord.js
+++ javascript/getMostAppearedWord.js
@@ -1,7 +1,7 @@
 var string = "The officials say tougher legislation is needed because some telecommunications companies in recent years have begun new services and made system upgrades that create technical obstacles to surveillance. They want to increase legal incentives and penalties aimed at pushing carriers like Verizon, AT&T, and Comcast to ensure that any network changes will not disrupt their ability to conduct wiretaps.An Obama administration task force that includes officials from the Justice and Commerce Departments, the F.B.I. and other agencies recently began working on draft legislation to strengthen and expand a 1994 law requiring carriers to make sure their systems can be wiretapped. There is not yet agreement over the details, according to officials familiar with the deliberations, but they said the administration intends to submit a package to Congress next year.To bolster their case, security agencies are citing two previously undisclosed episodes in which major carriers were stymied for weeks or even months when they tried to comply with court-approved wiretap orders in criminal or terrorism investigations, the officials said.";
 
 function getMostAppearedWord(string) {
-	var preTime = new Date().getTime();
+	var beginTime = new Date().getTime();
 	//取得所有单词匹配项
 	var matchs = string.match(/\s*\w+\s*/gi);
 	//数组转对象，用于统计单词出现的次数
@@ -29,7 +29,7 @@ function getMostAppearedWord(string) {
 			return pre
 		}
 	});
-	var nextTime = new Date().getTime();
-	console.log("找到字符串中出现最多的单词为:" + result.key + "，最多出现:" + result.value + "次;\n用时:" + (nextTime - preTime) + "毫秒")
+	var endTime = new Date().getTime();
+	console.log("找到字符串中出现最多的单词为:" + result.key + "，最多出现:" + result.value + "次;\n用时:" + (endTime - beginTime) + "毫秒")
 	return result
 }
