7e9c221c3fdbf3ac4db48694dcfbd2863f306c9d
meiseayoung <meiseayoung@gmail.com>
2017-03-20 11:08:01 +0800

    Create postMessage2iframeById.js

---
+++ javascript/postMessage2iframeById.js
@@ -0,0 +1,20 @@
+function getIframeById(id) {
+	var iframes = window.frames;
+	for (var i = 0; i < iframes.length; i++) {
+		if (iframes[i].frameElement.id === id){
+			return iframes[i];
+		}		
+	}
+};
+function postMessage2iframeById(message,id) {
+	var iframe = getIframeById(id);
+	var src = null;
+	if(iframe !== undefined){
+		src = iframe.src;
+		iframe.postMessage(message,src);
+	}else{
+    console.warn("没有找到指定iframe,操作失败");
+  }
+};
+
+export default postMessage2iframeById;
