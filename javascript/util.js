dd3b0a6185db5229d031d1b26208e42996b0b1ac
meiseayoung <meiseayoung@gmail.com>
2017-04-05 13:44:06 +0800

    Update util.js

--- javascript/util.js
+++ javascript/util.js
@@ -440,6 +440,29 @@ const util = {
   		var result = (isNaN(value) ? toNumber(value) : value)
   		return result;
   	},
+	 /**
+         * 函数柯里化相加
+         * @return result type:number 
+         **/
+	function curryAdd(){
+		  var result = 0;
+		  function _calc(args){
+		    var temp = 0;
+			for(let i=0;i<args.length;i++){
+			temp += args[i]
+		    };
+		    return temp;
+		  };
+		  result += _calc(arguments);
+		  var calc =  function(){
+		    result += _calc(arguments);
+			return calc;
+		  };
+		  calc.toString = function(){
+			return result;
+		  }
+		  return calc;
+	};
 	/**
 	 * 使用fetch请求数据	
 	 * @param         {[Object]}  
