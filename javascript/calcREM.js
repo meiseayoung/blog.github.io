c3e007710ff50f95c39329fc7c3a48cc93c37193
meiseayoung <meiseayoung@gmail.com>
2017-04-19 16:19:50 +0800

    Update calcREM.js

--- javascript/calcREM.js
+++ javascript/calcREM.js
@@ -9,7 +9,7 @@
 	var docEl = doc.documentElement,
         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
         recalc = function () {
-        	var mainWidth = document.body.getBoundingClientRect().width();
+        	var mainWidth = document.body.getBoundingClientRect().width;
         	if(mainWidth<1366)mainWidth = 1366;
         	if(mainWidth>1920)mainWidth = 1920;
 			if (!mainWidth) return;
