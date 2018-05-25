05e366474e561cbf10f470b7d51b6644ab2ddd24
meiseayoung <meiseayoung@gmail.com>
2018-05-25 10:03:44 +0800

    Create modernLazyLoad.js

---
+++ javascript/modernLazyLoad.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+	var lazyImages = Array.from(document.querySelectorAll("img.lazy"));
+	if ('IntersectionObserver' in window && 'IntersectionObserverEntry' in window && 'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
+		let lazyImageObserver = new IntersectionObserver((entries, observer) => {
+			console.log('entries', entries);
+			entries.forEach((entrie) => {
+				console.log('entries', entrie);
+				if (entrie.isIntersecting) {
+					let lazyImage = entrie.target;
+					lazyImage.src = lazyImage.dataset.srcset;
+					lazyImage.srcset = lazyImage.dataset.srcset;
+					lazyImage.classList.remove('laze');
+					lazyImageObsersver.unobserve(lazyImage);
+				}
+			})
+		})
+		lazyImages.forEach((lazyImage) => {
+			lazyImageObserver.observe(lazyImage);
+		});
+	}
+});
